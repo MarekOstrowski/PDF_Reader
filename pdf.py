c10ab7c901b22b137232dbe710da72c3174b5b7b
Marek Ostrowski <ostrowski.marek@boz.pl>
2024-08-05 11:55:05 +0200

    Initial Commit

---
+++ pdf.py
@@ -0,0 +1,14 @@
+import pypdf
+import os
+
+os.chdir("C:\\Users\\ostro\\Python\\PDF")
+pdfFile = ('dhl24.pdf')
+reader = pypdf.PdfReader(pdfFile)
+page1 = reader.pages[0]
+text = page1.extract_text()
+print(text)
+
+
+
+
+
